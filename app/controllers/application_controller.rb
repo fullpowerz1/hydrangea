5f682eadd58ace947be015b22e401df9eec12d9d
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-04-10 06:17:47 +0000

    tin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # ユーザー管理用のbefore_action
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:telephone_number])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  # 会員用1
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  root to: "homes#top"
+
+  # 会員用2
+  scope module: :public do
+  end
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :adimin do
+
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
